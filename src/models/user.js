4d07e4b6c185432efd0a28ebce7ddcd80cbcbf88
Le Vu <levu08072002@gmail.com>
2022-09-13 21:11:21 +0700

    TKW-4 signup API

--- src/models/user.js
+++ src/models/user.js
@@ -2,34 +2,24 @@ import mongoose, { Schema } from "mongoose";
 import validator from "validator";
 import bcrypt from 'bcrypt'
 const userSchema = new Schema({
-
-password: {
-    type: String,
-    required: true,
-    minLength: 8,
-    validate : (value) => {
-        return validator.isAlphanumeric(value)
-    }
-},
 name : {
     type : String,
     required : true,
     trim : true,
-},
-age : {
-    type : Number,
-    required : true,
-    validate(value){
-        if(value < 1){
-            throw new Error('Tuổi phải lớn hơn 0')
-        }
-    }
 },
 phoneNumber:{
     type : String,
     unique : true,
     validate : (value) =>{
         return /(84|0[3|5|7|8|9])+([0-9]{8})\b/g.test(value);
+    }
+},
+password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    validate : (value) => {
+        return validator.isAlphanumeric(value)
     }
 },
 role : {
@@ -39,10 +29,6 @@ role : {
 status : {
     type : Number,
     default : 0    
-},
-active : {
-    type : Boolean ,
-    default : false,
 },
 },{timestamps:true})
 
