ea34c07d29790372becd15f5a06b4541c6f4cdb1
Le Vu <levu08072002@gmail.com>
2022-09-17 12:25:49 +0700

    TKW-29 delete many service

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -1,15 +1,18 @@
+import service from "../models/service";
 import Service from "../models/service";
-export const createService = async(req, res) => {
-        try {
-            const service = await new Service(req.body).save();
-            return res.json(service);
-        } catch (error) {
-            return res.status(400).json({
-                message: error.message
-            })
-        }
+import { addNewDocument } from "../ultils/newDocument";
 
+export const createService = async(req, res) => {
+    try {
+        const service = await new Service(req.body).save();
+        return res.json(service);
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        })
     }
+
+}
 
 export const unactiveService = async (req,res) => {
     try {
@@ -20,5 +23,21 @@ export const unactiveService = async (req,res) => {
             error : error.message,
             message : 'Không thể lấy dữ liệu'
         })
+    }
+}
+
+export const deleteManyServices = async(req,res) => {
+    const services = req.body.services
+    try {
+            const deletedServices = await Service.deleteMany({ _id : services})
+            res.json({
+                deletedServices,
+                message : 'Xoá thành công'
+            })
+    } catch (error) {
+            return res.status(400).json({
+                error : error.message,
+                message : 'Xoá các dịch vụ không thành công'
+            })
     }
 }
--- src/routes/service.js
+++ src/routes/service.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createService, unactiveService } from "../controllers/service";
+import { createService, deleteManyServices, unactiveService } from "../controllers/service";
 
 const router = Router();
 router.post('/service', createService)
 router.get('/service/unactive-service',unactiveService)
+router.delete('/service/delete-many-service',deleteManyServices)
 export default router;
