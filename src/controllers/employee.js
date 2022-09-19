e916cf8b107db81ab1ed24d22fcb29f483c4ceda
TrieuVanBan <79831622+TrieuVanBan@users.noreply.github.com>
2022-09-19 19:27:16 +0700

    TKW-42 <be-detail-one-employee>

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -1,9 +1,9 @@
 import Employee from '../models/employee' 
 
-export const list = async (req, res) => {
+export const read = async (req, res) => {
     try {
-        const employees = await Employee.find().exec();
-        res.json(employees)
+        const employee = await Employee.findOne({id_: req.params.id}).exec();
+        res.json(employee)
     } catch (error) {
         res.status(400).json({
             message: "Khong co nhan vien nao",
