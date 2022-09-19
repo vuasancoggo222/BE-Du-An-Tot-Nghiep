7f0d4d249d347c0eb6a3903910e4450fad509659
tamltph13479 <thetam1002@gmail.com>
2022-09-19 21:22:26 +0700

    list-service-TKW-26

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -10,7 +10,18 @@ export const createService = async(req, res) => {
         }
 
     }
+//list service
+export const list = async(req, res) => {
+        try {
+            const ListServices = await Service.find();
+            res.json(ListServices);
+        } catch (error) {
+            res.status(400).json({
+                message: "Không tìm được dich vu anh eiii"
+            })
+        }
 
+    }
 export const unactiveService = async (req,res) => {
     try {
         const services = await Service.find({status : 0}).exec()
--- src/routes/service.js
+++ src/routes/service.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createService, unactiveService } from "../controllers/service";
+import { createService, list, unactiveService } from "../controllers/service";
 
 const router = Router();
+router.get('/service', list)
 router.post('/service', createService)
 router.get('/service/unactive-service',unactiveService)
 export default router;
