6ace25baf27479bc9d32e41d40601ce2a60a4ca2
Le Vu <levu08072002@gmail.com>
2022-09-20 21:33:23 +0700

    fix

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,7 +1,5 @@
 import { Router } from "express";
-import { create, list} from "../controllers/employee";
+import { create} from "../controllers/employee";
 const router = Router();
 router.post("/employees", create);
-router.get("/employees",list)
-
 export default router;
