dadffff8ea992b83bd70617a2b2e1194186661a6
Le Vu <levu08072002@gmail.com>
2022-09-20 21:52:14 +0700

    no msg

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {list,create} from "../controllers/employee";
+import {create,list} from "../controllers/employee";
 
 const router = Router();
 router.post("/employees", create);
