8da6f46a74bdfd12d6195db1d425f9d068747fee
Le Vu <levu08072002@gmail.com>
2022-09-21 01:44:22 +0700

    TKW-50 get employe by booking day

--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -1,5 +1,21 @@
 import Booking from "../models/booking";
+import Employee from "../models/employee";
 
+export const getEmployeeByDate = async (req, res) => {
+  const timeStamp = Number(req.query.date);
+  try {
+    const existEmployee = await Employee.find({
+      timeWork: { $elemMatch: { date: timeStamp } },
+    })
+      .select("-idCard")
+      .exec();
+    res.json(existEmployee);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
 export const createBooking = async (req, res) => {
   try {
     const booking = await new Booking(req.body).save();
