8f40a120248c2c431f0fc10ec04aa981b50e1569
Le Vu <levu08072002@gmail.com>
2022-09-20 21:05:13 +0700

    config router

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -2,6 +2,10 @@ import { Router } from "express";
 import { create} from "../controllers/employee";
 
 const router = Router();
-router.post("/employees", create);
 
+router.get("/employees", list);
+router.post("/employees", create);
+router.get("/employees", read);
+router.delete("/employees", remove);
+router.put("/employees", update);
 export default router;
