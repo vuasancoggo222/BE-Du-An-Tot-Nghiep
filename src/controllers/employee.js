0670d021788c9aee768337dda4245046c14ab7a0
Le Vu <levu08072002@gmail.com>
2022-09-21 20:37:38 +0700

    TKW-45 add new employee shift

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -1,42 +1,78 @@
-import Employee from '../models/employee' 
-
+import Employee from "../models/employee";
 export const list = async (req, res) => {
-    try {
-        const employees = await Employee.find({}).exec();
-        res.json(employees)
-    } catch (error) {
-        res.status(400).json({
-            message: error.message
-        })
-    }
-}
+  try {
+    const employees = await Employee.find({}).exec();
+    res.json(employees);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
 export const create = async (req, res) => {
-    try {
-        const employees = await new Employee(req.body).save()
-        res.json(employees)
-    } catch (error) {
-        res.status(400).json({
-            message: error.message
-        })
-    }
-}
+  try {
+    const employees = await new Employee(req.body).save();
+    res.json(employees);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
 export const update = async (req, res) => {
-    try {
-        const employee = await Employee.findOneAndUpdate({id_: req.params.id}, req.body, {new: true}).exec();
-        res.json(employee)
-    } catch (error) {
-        res.status(400).json({
-            message: error.message
-        })
-    }
-}
+  try {
+    const employee = await Employee.findOneAndUpdate(
+      { id_: req.params.id },
+      req.body,
+      { new: true }
+    ).exec();
+    res.json(employee);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
 export const read = async (req, res) => {
-    try {
-        const employee = await Employee.findOne({id_: req.params.id}).exec();
-        res.json(employee)
-    } catch (error) {
-        res.status(400).json({
-            message: error.message,
-        })
+  try {
+    const employee = await Employee.findOne({ id_: req.params.id }).exec();
+    res.json(employee);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+export const addEmployeeShift = async (req, res) => {
+  const employeeId = req.params.id;
+  const newShift = req.body;
+  const date = Number(newShift.date);
+  const shiftId = req.body.shiftId;
+  try {
+    console.log(shiftId);
+    const existShift = await Employee.find({
+      _id: employeeId,
+       "timeWork":{$elemMatch:{date,shiftId}}
+    }).exec();
+    if (existShift.length) {
+      return res.status(400).json({
+        message: "Ca làm đã tồn tại",
+      });
+    } else {
+      const employee = await Employee.findOneAndUpdate(
+        { _id: employeeId },
+        { $push: { timeWork: newShift } },
+        { new: true }
+      ).exec();
+      return res.json({
+        success: "Success",
+        employee,
+      });
     }
-}
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
