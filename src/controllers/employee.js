008e157478bca54e6ce1e452684a23b699fb79e7
Le Vu <levu08072002@gmail.com>
2022-09-23 01:25:08 +0700

    TKW-47 delete-employee-shift

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -75,3 +75,35 @@ export const addEmployeeShift = async (req, res) => {
     });
   }
 };
+
+export const deleteEmployeeShift = async (req,res) => {
+  const employeeId = req.params.id;
+  const date = Number(req.query.date);
+  const shiftId = req.query.shift;
+  try {
+    const existShift = await Employee.find({
+      _id: employeeId,
+       "timeWork" :{ $elemMatch: {date,shiftId} }
+    }).exec();
+    if (existShift.length) {
+      const employee = await Employee.findOneAndUpdate(
+        { _id: employeeId },
+        { $pull: { "timeWork": {shiftId,date} } },
+        { new: true,safe: true }
+      ).exec();
+      return res.json({
+        success: "Success",
+        employee,
+      });
+    }
+    else {
+      return res.status(400).json({
+        message: "Ca làm không tồn tại",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+}
--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {list,create,update,read, addEmployeeShift} from "../controllers/employee";
+import {list,create,update,read, addEmployeeShift, deleteEmployeeShift} from "../controllers/employee";
+import employee from "../models/employee";
 const router = Router();
 router.get("/employees",list)
 router.post("/employees",create)
 router.put("/employees/:id",update)
 router.get("/employees/:id",read)
 router.patch('/employees/new-employee-shift/:id',addEmployeeShift)
+router.patch('/employee/delete-employee-shift/:id',deleteEmployeeShift)
 export default router;
