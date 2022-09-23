34829638811b1b9ce103e95b14a6b14a65b4923f
Le Vu <levu08072002@gmail.com>
2022-09-24 04:05:49 +0700

    TKW-50 update

--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -1,29 +1,91 @@
-import { Mongoose } from "mongoose";
+import { mongoose } from "mongoose";
 import Booking from "../models/booking";
 import Employee from "../models/employee";
-
+import moment from "moment/moment";
 export const getEmployeeByDate = async (req, res) => {
   const timeStamp = Number(req.query.date);
-  const {employee} = req.query
+  const employeeId = req.query.employee
+  const currentTimeStamp = moment().startOf('day').unix()
   try {
-    const existEmployee = await Employee.find({
-        "_id" :  employee ,
-      "timeWork": { $elemMatch: { date: timeStamp }},
+   if(timeStamp && employeeId){
+    const existEmployee = await Employee.aggregate([
+      { $match : { _id : mongoose.Types.ObjectId(employeeId) }},
+      {
+        $project : {
+          "timeWork" : {
+            $filter : {
+              input: "$timeWork",
+              as: "data",
+              cond: {
+                  $eq: ["$$data.date", timeStamp],
+              }
+            }
+          }
+        }
+      }
+    ])
+    return res.json(existEmployee)
+   }
+   else if(timeStamp && !employeeId){
+    const existEmployee = await Employee.aggregate([
+      {
+        $project : {
+          "timeWork" : {
+            $filter : {
+              input: "$timeWork",
+              as: "data",
+              cond: {
+                  $eq: ["$$data.date", timeStamp],
+              }
+            }
+          }
+        }
+      }
+    ])
+    return res.json(existEmployee)
+   }
+   else if(!timeStamp && employeeId){
+    const existEmployee = await Employee.aggregate([
+      { $match : { _id : mongoose.Types.ObjectId(employeeId) }},
+      {
+        $project : {
+          "timeWork" : {
+            $filter : {
+              input: "$timeWork",
+              as: "data",
+              cond: {
+                  $gte : ["$$data.date",currentTimeStamp]
+              }
+            }
+          }
+        }
+      }
+    ])
+    return res.json(existEmployee)
+   }
+   else{
+    return res.status(400).json({
+      message : 'Vui lòng chọn thời gian hoặc nhân viên.'
     })
-      .select("-idCard")
-      .populate('timeWork.shiftId')
-      .exec();
-
-    return res.json(existEmployee);
+   }
   } catch (error) {
-    res.status(400).json({
-      message: error.message,
-    });
+    return res.status(400).json({
+      message : error.message
+    })
   }
 };
 export const createBooking = async (req, res) => {
   try {
-    const booking = await new Booking(req.body).save();
+   
+    const booking = await new Booking({
+      name : req.body.name,
+      phoneNumber : req.body.phoneNumber,
+      description : req.body.description,
+      serviceId : req.body.serviceId,
+      employeeId : req.query.employeeId,
+      date : Number(req.body.date),
+      shiftId: req.body.shiftId
+    }).save();
     return res.json(booking);
   } catch (error) {
     return res.status(400).json({
@@ -33,7 +95,7 @@ export const createBooking = async (req, res) => {
 };
 export const listBooking = async (req, res) => {
   try {
-    const booking = await Booking.find({}).populate("timeBook.shift").exec();
+    const booking = await Booking.find({}).populate("timeBook.shiftId").exec();
     return res.json(booking);
   } catch (error) {
     return res.status(400).json({
