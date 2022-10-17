94bbdbb44b3c5613dfd07e56be73b60855c6d5b6
Le Vu <levu08072002@gmail.com>
2022-10-18 00:50:20 +0700

    TKW-138 GET USER PROFILE

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -26,3 +26,12 @@ try {
 }
 }
 
+export const getUserProfile = async (req,res) => {
+ 
+try {
+    const userProfile = await User.findByIdAndUpdate({_id : req.user._id}).select('-password').exec()
+    return res.json(userProfile)
+} catch (error) {
+    return res.status(400).json(error.message)
+}
+}
