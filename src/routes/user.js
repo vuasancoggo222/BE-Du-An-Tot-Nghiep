a7772cb42b19d71dda7e9c9b3cfcd665227c24ba
Le Vu <levu08072002@gmail.com>
2022-10-22 15:50:10 +0700

    TKW-150 update user in admin

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 import {Router} from 'express'
-import { getUserProfile, listUser, updateProfile } from '../controllers/user'
+import { getUserProfile, listUser, updateProfile, updateUser } from '../controllers/user'
+import { isAdmin } from '../middlewares/checkRole'
 import { jwtVerifyToken } from '../middlewares/jwtVerifyToken'
 
 const router = new Router()
@@ -7,4 +8,5 @@ const router = new Router()
 router.get('/users',listUser)
 router.put('/user/my-profile/edit',jwtVerifyToken,updateProfile)
 router.get('/user/my-profile',jwtVerifyToken,getUserProfile)
+router.put('/user/edit/:id',jwtVerifyToken,isAdmin,updateUser)
 export default router
