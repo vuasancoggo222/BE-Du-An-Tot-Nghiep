4dd0321fb47077ed0371145e606660d8cbaae53a
vuasancoggo222 <levu08072002@gmail.com>
2022-11-10 01:42:07 +0700

    TKW-210 add timeStamp

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -79,54 +79,110 @@ export const readslug = async (req, res) => {
   }
 };
 
-export const groupAgeByService = async (req,res) => {
+export const groupAgeByService = async (req, res) => {
   try {
-    const serviceId = await Service.distinct('_id')
-    let groupAge = []
-    for (let svid of serviceId){
-      const service = await Service.findOne({_id : svid}).exec()
-      const group1 = await Booking.countDocuments({status : 4, serviceId: { $in: [svid] },age : {$gte : 0 ,$lte:16}})
-      const group2 = await Booking.countDocuments({status : 4, serviceId: { $in: [svid] },age : {$gte : 17 ,$lte:24}})
-      const group3 = await Booking.countDocuments({status : 4, serviceId: { $in: [svid] },age : {$gte : 25 ,$lte:34}})
-      const group4 = await Booking.countDocuments({status : 4, serviceId: { $in: [svid] },age : {$gte : 35 ,$lte:45}})
-      const group5 = await Booking.countDocuments({status : 4, serviceId: { $in: [svid] },age : {$gte : 46 ,$lte:60}})
-      const group6 = await Booking.countDocuments({status : 4, serviceId: { $in: [svid] },age : {$gt : 60 }})
-      
-      groupAge.push({
-        name : service.name,
-        data : [group1,group2,group3,group4,group5,group6]
-      })
-    }
-    return res.json({groupAge,categories : ["0-16","17-24","25-34","35-45","46-60",">60"]})
-  } catch (error) {
-    return res.status(400).json(error.message)
-  }
-}
+    const serviceId = await Service.distinct("_id");
+    let groupAge = [];
+    for (let svid of serviceId) {
+      const service = await Service.findOne({ _id: svid }).exec();
+      const group1 = await Booking.countDocuments({
+        status: 4,
+        serviceId: { $in: [svid] },
+        age: { $gte: 0, $lte: 16 },
+      });
+      const group2 = await Booking.countDocuments({
+        status: 4,
+        serviceId: { $in: [svid] },
+        age: { $gte: 17, $lte: 24 },
+      });
+      const group3 = await Booking.countDocuments({
+        status: 4,
+        serviceId: { $in: [svid] },
+        age: { $gte: 25, $lte: 34 },
+      });
+      const group4 = await Booking.countDocuments({
+        status: 4,
+        serviceId: { $in: [svid] },
+        age: { $gte: 35, $lte: 45 },
+      });
+      const group5 = await Booking.countDocuments({
+        status: 4,
+        serviceId: { $in: [svid] },
+        age: { $gte: 46, $lte: 60 },
+      });
+      const group6 = await Booking.countDocuments({
+        status: 4,
+        serviceId: { $in: [svid] },
+        age: { $gt: 60 },
+      });
 
-export const servicesStatistic = async (req,res) => {
-  try {
-    const serviceId = await Service.distinct('_id')
-    let services = []
-    let total = 0
-    for (let svid of serviceId){
-      const service = await Service.findOne({_id : svid}).exec()
-      const numberOfService = await Booking.countDocuments({serviceId : {$in : [svid]},status:4})
-      total += Number(numberOfService*service.price)
-      const serviceElement = {
-        name : service.name,
-        _id : service._id,
-        complete : numberOfService,
-        turnover : Number(numberOfService*service.price),
-      }
-      services.push(
-        serviceElement,
-      )
+      groupAge.push({
+        name: service.name,
+        data: [group1, group2, group3, group4, group5, group6],
+      });
     }
-    for(let i= 0 ; i < services.length ; i ++){
-      services[i].percentage = Number(services[i].turnover / total) * 100
-    }
-    return res.json(services)
+    return res.json({
+      groupAge,
+      categories: ["0-16", "17-24", "25-34", "35-45", "46-60", ">60"],
+    });
   } catch (error) {
-    return res.status(400).json(error.message)
+    return res.status(400).json(error.message);
   }
-}
+};
+
+export const servicesStatistic = async (req, res) => {
+  const { timeStart, timeEnd } = req.query;
+  try {
+    const serviceId = await Service.distinct("_id");
+    let services = [];
+    let total = 0;
+    if (!timeStart && !timeEnd) {
+      for (let svid of serviceId) {
+        const service = await Service.findOne({ _id: svid }).exec();
+        const numberOfService = await Booking.countDocuments({
+          serviceId: { $in: [svid] },
+          status: 4,
+        }).exec();
+        total += Number(numberOfService * service.price);
+        const serviceElement = {
+          name: service.name,
+          _id: service._id,
+          complete: numberOfService,
+          turnover: Number(numberOfService * service.price),
+        };
+        services.push(serviceElement);
+      }
+      for (let i = 0; i < services.length; i++) {
+        services[i].percentage = Number(services[i].turnover / total) * 100;
+      }
+      return res.json(services);
+    } else if (timeStart && timeEnd) {
+      console.log(new Date(Number(1667412158494) * 1000).toISOString());
+      for (let svid of serviceId) {
+        const service = await Service.findOne({ _id: svid }).exec();
+        const numberOfService = await Booking.countDocuments({
+          serviceId: { $in: [svid] },
+          status: 4,
+          date: {
+            $gte: new Date(Number(timeStart) * 1000).toISOString(),
+            $lte: new Date(Number(timeEnd) * 1000).toISOString(),
+          },
+        }).exec();
+        total += Number(numberOfService * service.price);
+        const serviceElement = {
+          name: service.name,
+          _id: service._id,
+          complete: numberOfService,
+          turnover: Number(numberOfService * service.price),
+        };
+        services.push(serviceElement);
+      }
+      for (let i = 0; i < services.length; i++) {
+        services[i].percentage = Number(services[i].turnover / total) * 100;
+      }
+      return res.json(services);
+    }
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
