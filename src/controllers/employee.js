54a9ee6c8a8b3bbf3fcc7ce3599e24d05d3c714e
vuasancoggo222 <levu08072002@gmail.com>
2022-11-14 01:21:08 +0700

    TKW-218-BE-STATISTICS FOR EMPLOYEE

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -67,17 +67,24 @@ export const deleteEmployee = async (req, res) => {
 export const employeeOrderStatistics = async (req, res) => {
   let statistics = [];
   try {
-    const employee = await Employee.find({}).select('-idCard').exec();
-    const getTotal = await Booking.aggregate([{$match : { status : 4 }},{$group : {_id : null, sum : {$sum : "$bookingPrice"}}}])
+    const employee = await Employee.find({}).select("-idCard").exec();
+    const getTotal = await Booking.aggregate([
+      { $match: { status: 4 } },
+      { $group: { _id: null, sum: { $sum: "$bookingPrice" } } },
+    ]);
     const totalTurnOver = getTotal[0].sum;
     for (let i = 0; i < employee.length; i++) {
-      const finishedBooking = await Booking.find({employeeId : employee[i]._id,status : 4}).exec()
+      const finishedBooking = await Booking.find({
+        employeeId: employee[i]._id,
+        status: 4,
+      }).exec();
       const turnover = finishedBooking.reduce(
-        (previousValue, currentValue) => previousValue + currentValue.bookingPrice,
+        (previousValue, currentValue) =>
+          previousValue + currentValue.bookingPrice,
         0
       );
 
-      const percentage = Number((turnover / totalTurnOver))*100
+      const percentage = Number(turnover / totalTurnOver) * 100;
       console.log(percentage);
       const totalBooking = await Booking.countDocuments({
         employeeId: employee[i]._id,
@@ -103,7 +110,7 @@ export const employeeOrderStatistics = async (req, res) => {
         status: 4,
       }).exec();
       const status = {
-        employee : employee[i],
+        employee: employee[i],
         totalBooking,
         unConfimred,
         confirmed,
@@ -111,14 +118,14 @@ export const employeeOrderStatistics = async (req, res) => {
         waitToPay,
         canceled,
         turnover,
-        percentage
+        percentage,
       };
       statistics.push(status);
     }
     res.json({
       statistics,
-      totalEmployee : employee.length,
-      totalTurnOver
+      totalEmployee: employee.length,
+      totalTurnOver,
     });
   } catch (error) {
     return res.status(400).json({
@@ -126,3 +133,44 @@ export const employeeOrderStatistics = async (req, res) => {
     });
   }
 };
+
+export const statisticsForOneEmployee = async(req,res) =>{
+  const id =  req.query.id 
+  const getTotal = await Booking.aggregate([
+    { $match: { status: 4 } },
+    { $group: { _id: null, sum: { $sum: "$bookingPrice" } } },
+  ]);
+  const totalTurnOver = getTotal[0].sum;
+  try {
+    const information = await Employee.findOne({_id : id}).exec()
+    const totalBooking = await Booking.countDocuments({employeeId : id}).exec()
+    const unConfimred = await Booking.countDocuments({employeeId : id,status:0}).exec()
+    const confirmed = await Booking.countDocuments({employeeId : id,status:1}).exec()
+    const canceled = await Booking.countDocuments({employeeId : id,status:2}).exec()
+    const waitToPay = await Booking.countDocuments({employeeId : id,status:3}).exec()
+    const finished = await Booking.countDocuments({employeeId : id,status:4}).exec()
+    const finishedBooking = await Booking.find({
+      employeeId: id,
+      status: 4,
+    }).exec();
+    const turnover = finishedBooking.reduce(
+      (previousValue, currentValue) =>
+        previousValue + currentValue.bookingPrice,
+      0
+    );
+    const percentage = Number(turnover / totalTurnOver) * 100;
+    res.json({
+      information,
+      totalBooking,
+      unConfimred,
+      confirmed,
+      canceled,
+      waitToPay,
+      finished,
+      turnover,
+      percentage
+    })
+  } catch (error) {
+    return res.json(error.message)
+  }
+}
