5bef071abfdb1835dd4ea57d1a704ce8de8ffaaf
quan08 <113536053+DucQuan88@users.noreply.github.com>
2022-12-18 22:32:32 +0700

    fix

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import {list,create,update,read,deleteEmployee, employeeOrderStatistics, statisticsForOneEmployee} from "../controllers/employee";
+import { list, create, update, read, deleteEmployee, employeeOrderStatistics, statisticsForOneEmployee } from "../controllers/employee";
 import { isAdmin, isEmployee } from "../middlewares/checkRole";
 import { jwtVerifyToken } from "../middlewares/jwtVerifyToken";
 const router = Router();
-router.get("/employees",list)
-router.post("/employees",jwtVerifyToken,isAdmin,create)    
-router.patch("/employees/:id",jwtVerifyToken,isAdmin,update)
-router.delete("/employees/:id",jwtVerifyToken,isAdmin,deleteEmployee)
-router.get("/employees/:id",jwtVerifyToken,isAdmin,read)
-router.get("/employee/order-statistics",jwtVerifyToken,isAdmin,employeeOrderStatistics)
-router.get("/statistics-for-employee/:id",jwtVerifyToken,isEmployee,statisticsForOneEmployee)
+router.get("/employees", list)
+router.post("/employees", jwtVerifyToken, isAdmin, create)
+router.patch("/employees/:id", jwtVerifyToken, isAdmin, update)
+router.delete("/employees/:id", jwtVerifyToken, isAdmin, deleteEmployee)
+router.get("/employees/:id", jwtVerifyToken, read) //Nhân viên phải dùng hàm này
+router.get("/employee/order-statistics", jwtVerifyToken, isAdmin, employeeOrderStatistics)
+router.get("/statistics-for-employee/:id", jwtVerifyToken, statisticsForOneEmployee) //Amdin cũng xem được bảng booking nhân viên
 export default router;
