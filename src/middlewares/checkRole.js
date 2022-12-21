09db14f903cb6de3bec3b4bcdae478d15d73e770
vuasancoggo222 <levu08072002@gmail.com>
2022-12-21 12:37:42 +0700

    up

--- src/middlewares/checkRole.js
+++ src/middlewares/checkRole.js
@@ -17,7 +17,7 @@ export const isEmployee = (req, res, next) => {
 };
 
 export const isAdminOrEmployee = (req, res, next) => {
-  if (req.user.role !== 1 || req.user.role !==2) {
+  if (req.user.role == 0) {
     return res.status(401).json({
       message: "Bạn không phải nhân viên hoặc admin.",
     });
