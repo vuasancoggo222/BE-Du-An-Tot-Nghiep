1baddf16afb7eb4ba7ca9b39b5e872cf8d2775c1
TrieuVanBan <79831622+TrieuVanBan@users.noreply.github.com>
2022-12-25 10:37:56 +0700

    notification

--- src/controllers/notification.js
+++ src/controllers/notification.js
@@ -2,17 +2,24 @@ import Notification from "../models/notification";
 
 export const getListAdminNotification = async () => {
   try {
-    const listNotification = await Notification.find({notificationType : 'admin'})
+    const listNotification = await Notification.find({
+      notificationType: "admin",
+    })
       .populate("bookingId")
       .sort({ createdAt: -1 })
       .exec();
-    const unRead = await Notification.countDocuments({ readed: false, notificationType : 'admin' }).exec();
-     return {
+
+    const unRead = await Notification.countDocuments({
+      readed: false,
+      notificationType: "admin",
+    }).exec();
+    console.log(unRead);
+    return {
       notification: listNotification,
       unRead,
     };
   } catch (error) {
-    return error
+    return error;
   }
 };
 
@@ -27,27 +34,39 @@ export const newNotification = async (data) => {
 
 export const getUserListNotification = async (userId) => {
   try {
-    const userListNotification = await Notification.find({userId}).populate("bookingId").sort({ createdAt: -1 }).exec();
-    const unRead = userListNotification.filter(item => item.userId == userId && item.readed == false)
+    const userListNotification = await Notification.find({ userId })
+      .populate("bookingId")
+      .sort({ createdAt: -1 })
+      .exec();
+    const unRead = userListNotification.filter(
+      (item) => item.userId == userId && item.readed == false
+    );
     return {
       notification: userListNotification,
-      unRead : unRead.length,
-    }
+      unRead: unRead.length,
+    };
   } catch (error) {
-    return error
+    return error;
   }
 };
 
 export const getEmployeeListNotification = async (employeeId) => {
   try {
-    const employeeList = await Notification.find({employeeId}).populate("bookingId").sort({ createdAt: -1 }).exec();
-    const unRead = employeeList.filter(item => item.employeeId == employeeId && item.readed == false)
+    const employeeList = await Notification.find({ employeeId })
+      .populate("bookingId")
+      .sort({ createdAt: -1 })
+      .exec();
+    const unRead = await Notification.countDocuments({
+      employeeId,
+      readed: false,
+    }).exec();
+
     return {
       notification: employeeList,
-      unRead : unRead.length,
-    }
+      unRead,
+    };
   } catch (error) {
-    return error
+    return error;
   }
 };
 
@@ -59,18 +78,18 @@ export const readNotification = async (req, res) => {
       { new: true }
     ).exec();
     console.log(readNotification);
-      if(readNotification.notificationType == "admin"){
-        const data = await getListAdminNotification()
-        return res.json(data)
-      }
-      else if(readNotification.notificationType == "user"){
-        const data = await getUserListNotification(readNotification.userId)
-        return res.json(data)
-      }
-      else if(readNotification.notificationType == "employee"){
-        const data = await getEmployeeListNotification(readNotification.employeeId)
-        return res.json(data)
-      }
+    if (readNotification.notificationType == "admin") {
+      const data = await getListAdminNotification();
+      return res.json(data);
+    } else if (readNotification.notificationType == "user") {
+      const data = await getUserListNotification(readNotification.userId);
+      return res.json(data);
+    } else if (readNotification.notificationType == "employee") {
+      const data = await getEmployeeListNotification(
+        readNotification.employeeId
+      );
+      return res.json(data);
+    }
   } catch (error) {
     res.status(400).json(error.message);
   }
